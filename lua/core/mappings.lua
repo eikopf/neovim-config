ed8ff93710d372e4fd7566958d4b08411e7ae7ec
Oliver Wooding <oliverwooding@icloud.com>
2023-08-06 20:39:09 +0200

    modified plugins and mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -9,12 +9,29 @@ require("which-key").register({
   ["<leader>"] = {
     ["."] = { "<cmd>Telescope find_files<cr>", "Search Files" },
     [","] = { "<cmd>Telescope buffers<cr>", "Search Buffers" },
-    f = { name = "+file" },
+    b = {
+      name = "+buffer",
+    },
+
+    f = { name = "+file", },
 
     g = { 
       name = "+git",
+      A = { "<cmd>Git add -A<cr>", "Stage All" },
+      c = { "<cmd>Telescope git_branches<cr>", "Checkout" },
+      C = { "<cmd>Git commit<cr>", "Commit" },
+      d = { "<cmd>Git diff<cr>", "Diff" },
       g = { "<cmd>Git<cr>", "Status" },
-      h = { "<cmd>Flog<cr>", "Show History" },
+      h = {
+        name = "+hunk",
+        n = { "<cmd>Gitsigns next_hunk<cr>", "Next" },
+        p = { "<cmd>Gitsigns prev_hunk<cr>", "Previous" },
+        P = { "<cmd>Gitsigns preview_hunk<cr>", "Preview" },
+      },
+      l = { "<cmd>Git log<cr>", "Log" },
+      p = { "<cmd>Git push<cr>", "Push" },
+      r = { "<cmd>Git reset<cr>", "Reset" },
+      t = { "<cmd>Flog<cr>", "Show Tree" },
     },
 
     l = { 
@@ -43,6 +60,7 @@ require("which-key").register({
       l = { "<cmd>Lazy<cr>", "Lazy" },
       m = { "<cmd>Mason<cr>", "Mason" },
       p = { "<cmd>Telescope projects<cr>", "Projects" },
+      t = { "<cmd>terminal<cr>", "Terminal" },
     },
 
     s = { 
