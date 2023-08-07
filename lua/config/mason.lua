b03edb4e8de5b46d939b46c5eefd4ce7ec844c85
Oliver Wooding <oliverwooding@icloud.com>
2023-08-07 16:59:09 +0200

    fixed DAP

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -35,7 +35,7 @@ local rust_tools_config = {
     local install_root_dir = vim.fn.stdpath "data" .. "/mason"
 	local extension_path = install_root_dir .. "/packages/codelldb/extension/"
 	local codelldb_path = extension_path .. "adapter/codelldb"
-	local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+	local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
 
 	return {
 	  adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
