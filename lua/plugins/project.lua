26a4896abbd4a2c95a82b317a2d2eba9c81bb994
Oliver Wooding <oliverwooding@icloud.com>
2023-08-09 17:45:22 +0200

    changes to lualine and project configs

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -18,6 +18,13 @@ return {
 
       silent_chdir = true, -- false makes this useful but annoying
     }
+
+    -- enable telescope integration
     require('telescope').load_extension('projects')
   end,
 }
+
+-- notes:
+-- annoyingly, project.nvim will not automatically switch the cwd unless
+-- it finds one of the pattern files, even when switching into a manually
+-- added project via the telescope integration. this might be worth a PR.
